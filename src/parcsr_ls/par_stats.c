71a2018eb03141b366719719fff60b44952d00ec
Gerald Collom <collom1@lassen708.coral.llnl.gov>
2023-03-30 12:19:54 -0700

    First commit, modifying for experiment

--- src/parcsr_ls/par_stats.c
+++ src/parcsr_ls/par_stats.c
@@ -7,6 +7,7 @@
 
 #include "_hypre_parcsr_ls.h"
 #include "par_amg.h"
+#include <stdbool.h>
 
 /*****************************************************************************
  *
@@ -1322,16 +1323,18 @@ HYPRE_Int hypre_BoomerAMGMatTimes(void* data)
    hypre_ParCSRCommPkg* comm_pkg;
    A_array = hypre_ParAMGDataAArray(amg_data);
 
-   MPIX_Comm* neighbor_comm;
-   MPIX_Request* request;
    MPI_Status status;
 
    double t0, tfinal;
+   int timing_iters = 1000;
+   int i, j, k;
 
-   for (int i = 0; i < num_levels; i++)
+   for (i = 0; i < num_levels; i++)
    {
       comm_pkg = hypre_ParCSRMatrixCommPkg(A_array[i]);
       MPI_Comm comm = hypre_ParCSRCommPkgComm(comm_pkg);
+      hypre_ParCSRPersistentCommHandle *comm_handle = hypre_ParCSRCommPkgGetPersistentCommHandle(1, comm_pkg);
+      double *send_data = (double *) comm_handle->send_data_buffer;
 
       int rank, num_procs;
       MPI_Comm_rank(comm, &rank);
@@ -1339,44 +1342,118 @@ HYPRE_Int hypre_BoomerAMGMatTimes(void* data)
 
       int nsends = hypre_ParCSRCommPkgNumSends(comm_pkg);
       int nrecvs = hypre_ParCSRCommPkgNumRecvs(comm_pkg);
+      int num_requests = nsends + nrecvs;
       int* sdispls = hypre_ParCSRCommPkgSendMapStarts(comm_pkg);
       int* rdispls = hypre_ParCSRCommPkgRecvVecStarts(comm_pkg);
+
+      if (rank == 0) printf("----------------------------------\n");
+      // Standard hypre
+      hypre_MPI_Request *requests;
+      if (num_requests > 0)
+         requests = hypre_CTAlloc(hypre_MPI_Request, num_requests, HYPRE_MEMORY_HOST);
+      double *sendbuf = NULL;
+      double *recvbuf1 = NULL;
+      if (nsends)
+      {
+         sendbuf = (double*)malloc(sdispls[nsends]*sizeof(double));
+         for (j = 0; j < sdispls[nsends]; j++)
+            sendbuf[j] = send_data[j];
+      }
+      if (nrecvs)
+      {
+         recvbuf1 = (double*)malloc(rdispls[nrecvs]*sizeof(double));
+      }
+
+      MPI_Barrier(comm);
+      t0 = MPI_Wtime();
+      for (j = 0; j < nrecvs; ++j)
+      {
+         HYPRE_Int ip = hypre_ParCSRCommPkgRecvProc(comm_pkg, j);
+         HYPRE_Int vec_start = hypre_ParCSRCommPkgRecvVecStart(comm_pkg, j);
+         HYPRE_Int vec_len = hypre_ParCSRCommPkgRecvVecStart(comm_pkg, j + 1) - vec_start;
+         hypre_MPI_Recv_init( recvbuf1 + vec_start, vec_len, MPI_DOUBLE,
+                              ip, 0, comm, requests + j );
+      }
+      for (j = 0; j < nsends; ++j)
+      {
+         HYPRE_Int ip = hypre_ParCSRCommPkgSendProc(comm_pkg, j);
+         HYPRE_Int vec_start = hypre_ParCSRCommPkgSendMapStart(comm_pkg, j);
+         HYPRE_Int vec_len = hypre_ParCSRCommPkgSendMapStart(comm_pkg, j + 1) - vec_start;
+         hypre_MPI_Send_init( sendbuf + vec_start, vec_len, MPI_DOUBLE,
+                              ip, 0, comm, requests + nrecvs + j );
+      }
+      tfinal = MPI_Wtime() - t0;
+      MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+      if (rank == 0) printf("Standard init time: %e\n", t0);
+
+
+      // warmup exchange
+      if (num_requests > 0) {
+         hypre_MPI_Startall(num_requests, requests);
+         hypre_MPI_Waitall(num_requests, requests, hypre_MPI_STATUSES_IGNORE);
+      }
+      MPI_Barrier(comm);
+      t0 = MPI_Wtime();
+      if (num_requests > 0) {
+         for (j = 0; j < timing_iters; ++j) { 
+            hypre_MPI_Startall(num_requests, requests);
+            hypre_MPI_Waitall(num_requests, requests, hypre_MPI_STATUSES_IGNORE);
+         }
+      }
+      tfinal = MPI_Wtime() - t0;
+      MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+      if (rank == 0) printf("Standard start/wait time: %e\n", t0/timing_iters);
+
+      // warmup exchange
+      if (num_requests > 0) {
+         hypre_ParCSRPersistentCommHandleStart(comm_handle, HYPRE_MEMORY_HOST, sendbuf);
+         hypre_ParCSRPersistentCommHandleWait(comm_handle, HYPRE_MEMORY_HOST, recvbuf1);
+      }
+      MPI_Barrier(comm);
+      t0 = MPI_Wtime();
+      if (num_requests > 0) {
+         for (j = 0; j < timing_iters; ++j) { 
+            hypre_ParCSRPersistentCommHandleStart(comm_handle, HYPRE_MEMORY_HOST, sendbuf);
+            hypre_ParCSRPersistentCommHandleWait(comm_handle, HYPRE_MEMORY_HOST, recvbuf1);
+         }
+      }
+      tfinal = MPI_Wtime() - t0;
+      MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+      if (rank == 0) printf("Standard start/wait time 2: %e\n", t0/timing_iters);
+
+      free(recvbuf1);
+      free(sendbuf);
+      for (j = 0; j < num_requests; j++) {
+         hypre_MPI_Request_free(&requests[j]); 
+      }
+
+
+      // Unoptimized Neighborhood Collective
+      MPIX_Comm* neighbor_comm;
+      MPIX_Request* request;
       int* sendcounts = NULL;
       int* recvcounts = NULL;
       if (nsends)
          sendcounts = (int*)malloc(nsends*sizeof(int));
       if (nrecvs)
          recvcounts = (int*)malloc(nrecvs*sizeof(int));
-      for (int i = 0; i < nsends; i++)
-         sendcounts[i] = sdispls[i+1] - sdispls[i];
-      for (int i = 0; i < nrecvs; i++)
-         recvcounts[i] = rdispls[i+1] - rdispls[i];
-      long* global_sidx = NULL;
-      double *sendbuf = NULL;
-      long* global_ridx = NULL;
-      double* recvbuf = NULL;
+      for (j = 0; j < nsends; j++)
+         sendcounts[j] = sdispls[j+1] - sdispls[j];
+      for (j = 0; j < nrecvs; j++)
+         recvcounts[j] = rdispls[j+1] - rdispls[j];
+      double* recvbuf2 = NULL;
       if (nsends)
       {
-         global_sidx = (long*)malloc(sdispls[nsends]*sizeof(long));
          sendbuf = (double*)malloc(sdispls[nsends]*sizeof(double));
+         for (j = 0; j < sdispls[nsends]; j++)
+            sendbuf[j] = send_data[j];
       }
       if (nrecvs)
       {
-         global_ridx = (long*)malloc(rdispls[nrecvs]*sizeof(long));
-         recvbuf = (double*)malloc(rdispls[nrecvs]*sizeof(double));
+         recvbuf2 = (double*)malloc(rdispls[nrecvs]*sizeof(double));
       }
 
-      int* send_map_elmts = hypre_ParCSRCommPkgSendMapElmts(comm_pkg);
-      int first_col_diag = hypre_ParCSRMatrixFirstColDiag(A_array[i]);
-      int* col_map_offd = hypre_ParCSRMatrixColMapOffd(A_array[i]);
-      for (int i = 0; i < nsends; i++)
-         for (int j = sdispls[i]; j < sdispls[i+1]; j++)
-            global_sidx[j] = send_map_elmts[j] + first_col_diag;
-      for (int i = 0; i < nrecvs; i++)
-         for (int j = rdispls[i]; j < rdispls[i+1]; j++)
-            global_ridx[j] = col_map_offd[j];
-
-      // Dist Graph Create Adjacent
+      // Graph Create
       MPI_Barrier(comm);
       t0 = MPI_Wtime();
       MPIX_Dist_graph_create_adjacent(
@@ -1390,24 +1467,179 @@ HYPRE_Int hypre_BoomerAMGMatTimes(void* data)
             MPI_INFO_NULL,
             0,
             &neighbor_comm);
-//      update_locality(neighbor_comm, 4);
-
       tfinal = MPI_Wtime() - t0;
       MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0,
             hypre_ParCSRCommPkgComm(comm_pkg));
       if (rank == 0) printf("Dist Graph Create Time %e\n", t0);
 
-      // Neighbor Alltoallv Init Time
+      // Collective Init
+      MPI_Barrier(comm);
+      t0 = MPI_Wtime();
+      MPIX_Neighbor_alltoallv_init(
+            sendbuf,
+            sendcounts, 
+            hypre_ParCSRCommPkgSendMapStarts(comm_pkg),
+            MPI_DOUBLE, 
+            recvbuf2,
+            recvcounts,
+            hypre_ParCSRCommPkgRecvVecStarts(comm_pkg),
+            MPI_DOUBLE,
+            neighbor_comm,
+            MPI_INFO_NULL,
+            &request);
+      tfinal = MPI_Wtime() - t0;
+      MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+      if (rank == 0) printf("unoptimized init time: %e\n", t0);
+
+      // warmup exchange
+      MPIX_Start(request);
+      MPIX_Wait(request, &status);
+
+      MPI_Barrier(comm);
+      t0 = MPI_Wtime();
+      for (j = 0; j < timing_iters; ++j) { 
+         MPIX_Start(request);
+         MPIX_Wait(request, &status);
+      }
+      tfinal = MPI_Wtime() - t0;
+      MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+      if (rank == 0) printf("unoptimized Start/Wait time: %e\n", t0/timing_iters);
+
+      // buffer check
+      /*bool pass = NULL;
+      bool pass_loc = true;
+      for (j = 0; j < rdispls[nrecvs]; j++) {
+          if (recvbuf1[j] != recvbuf2[j]) {
+              pass_loc = false;
+              hypre_printf("rank %d expected %f, got %f\n", rank, recvbuf1[j], recvbuf2[j]);
+          }
+      }
+      if (!pass_loc) { hypre_printf("rank %d failed\n", rank); }
+      MPI_Reduce(&pass_loc, &pass, 1, MPI_INT, MPI_LAND, 0, comm);
+      if (rank == 0) { hypre_printf("verification for unopt: %s\n", pass ? "pass" : "fail"); }
+      */
+      free(sendcounts);
+      free(recvcounts);
+      free(recvbuf2);
+      free(sendbuf);
+      MPIX_Request_free(request);
+
+      // Partially Optimized Neighborhood Collective
+      if (nsends)
+         sendcounts = (int*)malloc(nsends*sizeof(int));
+      if (nrecvs)
+         recvcounts = (int*)malloc(nrecvs*sizeof(int));
+      for (j = 0; j < nsends; j++)
+         sendcounts[j] = sdispls[j+1] - sdispls[j];
+      for (j = 0; j < nrecvs; j++)
+         recvcounts[j] = rdispls[j+1] - rdispls[j];
+      if (nsends)
+      {
+         sendbuf = (double*)malloc(sdispls[nsends]*sizeof(double));
+         for (j = 0; j < sdispls[nsends]; j++)
+            sendbuf[j] = send_data[j];
+      }
+      if (nrecvs)
+      {
+         recvbuf2 = (double*)malloc(rdispls[nrecvs]*sizeof(double));
+      }
+      // partial optimized Neighbor Alltoallv Init Time
+      MPI_Barrier(comm);
+      t0 = MPI_Wtime();
+      MPIX_Neighbor_part_locality_alltoallv_init(
+            sendbuf,
+            sendcounts, 
+            hypre_ParCSRCommPkgSendMapStarts(comm_pkg),
+            MPI_DOUBLE, 
+            recvbuf2,
+            recvcounts,
+            hypre_ParCSRCommPkgRecvVecStarts(comm_pkg),
+            MPI_DOUBLE,
+            neighbor_comm,
+            MPI_INFO_NULL,
+            &request);
+      tfinal = MPI_Wtime() - t0;
+      MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0,
+            hypre_ParCSRCommPkgComm(comm_pkg));
+      if (rank == 0) printf("Neighbor part nap Alltoallv Init Time %e\n", t0);
+
+      // Time Communication
+      // warmup exchange
+      MPIX_Start(request);
+      MPIX_Wait(request, &status);
+      MPI_Barrier(comm);
+      t0 = MPI_Wtime();
+      for (j = 0; j < timing_iters; ++j) { 
+         MPIX_Start(request);
+         MPIX_Wait(request, &status);
+      }
+      tfinal = MPI_Wtime() - t0;
+      MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0,
+            hypre_ParCSRCommPkgComm(comm_pkg));
+      if (rank == 0) printf("part nap Start/Wait Time %e\n", t0/timing_iters);
+
+      // buffer check
+      /*pass = NULL;
+      pass_loc = true;
+      for (j = 0; j < rdispls[nrecvs]; j++) {
+          if (recvbuf1[j] != recvbuf2[j]) {
+              pass_loc = false;
+              hypre_printf("rank %d expected %f, got %f\n", rank, recvbuf1[j], recvbuf2[j]);
+          }
+      }
+      if (!pass_loc) { hypre_printf("rank %d failed\n", rank); }
+      MPI_Reduce(&pass_loc, &pass, 1, MPI_INT, MPI_LAND, 0, comm);
+      if (rank == 0) { hypre_printf("verification for part: %s\n", pass ? "pass" : "fail"); }*/
+
+      free(sendcounts);
+      free(recvcounts);
+      free(recvbuf2);
+      free(sendbuf);
+      MPIX_Request_free(request);
+
+      // Fully Optimized Neighborhood Collective
+      if (nsends)
+         sendcounts = (int*)malloc(nsends*sizeof(int));
+      if (nrecvs)
+         recvcounts = (int*)malloc(nrecvs*sizeof(int));
+      for (j = 0; j < nsends; j++)
+         sendcounts[j] = sdispls[j+1] - sdispls[j];
+      for (j = 0; j < nrecvs; j++)
+         recvcounts[j] = rdispls[j+1] - rdispls[j];
+      long* global_sidx = NULL;
+      long* global_ridx = NULL;
+      if (nsends)
+      {
+         global_sidx = (long*)malloc(sdispls[nsends]*sizeof(long));
+         sendbuf = (double*)malloc(sdispls[nsends]*sizeof(double));
+         for (j = 0; j < sdispls[nsends]; j++)
+            sendbuf[j] = send_data[j];
+      }
+      if (nrecvs)
+      {
+         global_ridx = (long*)malloc(rdispls[nrecvs]*sizeof(long));
+         recvbuf2 = (double*)malloc(rdispls[nrecvs]*sizeof(double));
+      }
+      int* send_map_elmts = hypre_ParCSRCommPkgSendMapElmts(comm_pkg);
+      int first_col_diag = hypre_ParCSRMatrixFirstColDiag(A_array[i]);
+      int* col_map_offd = hypre_ParCSRMatrixColMapOffd(A_array[i]);
+      for (j = 0; j < nsends; j++)
+         for (k = sdispls[j]; k < sdispls[j+1]; k++)
+            global_sidx[k] = send_map_elmts[k] + first_col_diag;
+      for (j = 0; j < nrecvs; j++)
+         for (k = rdispls[j]; k < rdispls[j+1]; k++)
+            global_ridx[k] = col_map_offd[k];
+
+      // fully optimized Neighbor Alltoallv Init Time
       MPI_Barrier(comm);
       t0 = MPI_Wtime();
       MPIX_Neighbor_locality_alltoallv_init(
-      //MPIX_Neighbor_part_locality_alltoallv_init(
             sendbuf,
             sendcounts, 
             hypre_ParCSRCommPkgSendMapStarts(comm_pkg),
             global_sidx,
             MPI_DOUBLE, 
-            recvbuf,
+            recvbuf2,
             recvcounts,
             hypre_ParCSRCommPkgRecvVecStarts(comm_pkg),
             global_ridx,
@@ -1418,30 +1650,46 @@ HYPRE_Int hypre_BoomerAMGMatTimes(void* data)
       tfinal = MPI_Wtime() - t0;
       MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0,
             hypre_ParCSRCommPkgComm(comm_pkg));
-      if (rank == 0) printf("Neighbor Alltoallv Init Time %e\n", t0);
+      if (rank == 0) printf("full nap Neighbor Alltoallv Init Time %e\n", t0);
 
       // Time Communication
-      MPI_Barrier(comm);
-      t0 = MPI_Wtime();
+      // warmup exchange
       MPIX_Start(request);
       MPIX_Wait(request, &status);
+      MPI_Barrier(comm);
+      t0 = MPI_Wtime();
+      for (j = 0; j < timing_iters; ++j) { 
+         MPIX_Start(request);
+         MPIX_Wait(request, &status);
+      }
       tfinal = MPI_Wtime() - t0;
       MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0,
             hypre_ParCSRCommPkgComm(comm_pkg));
-      if (rank == 0) printf("Start/Wait Time %e\n", t0);
+      if (rank == 0) printf("full nap Start/Wait Time %e\n", t0/timing_iters);
+
+      // buffer check
+      /*pass = NULL;
+      pass_loc = true;
+      for (j = 0; j < rdispls[nrecvs]; j++) {
+          if (recvbuf1[j] != recvbuf2[j]) {
+              pass_loc = false;
+              hypre_printf("rank %d expected %e, got %e. Diff: %e\n", rank, recvbuf1[j], recvbuf2[j], recvbuf1[j] - recvbuf2[j]);
+          }
+      }
+      if (!pass_loc) { hypre_printf("rank %d failed\n", rank); }
+      MPI_Reduce(&pass_loc, &pass, 1, MPI_INT, MPI_LAND, 0, comm);
+      if (rank == 0) { hypre_printf("verification for full opt: %s\n", pass ? "pass" : "fail"); }*/
 
       free(sendcounts);
       free(recvcounts);
       free(global_sidx);
       free(global_ridx);
       free(sendbuf);
-      free(recvbuf);
+      free(recvbuf2);
 
       MPIX_Request_free(request);
       MPIX_Comm_free(neighbor_comm);
    }
-
-
 }
 
 
